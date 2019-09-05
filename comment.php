898f988a34d321720e02c3bbec81d066149fa5e0
VanjaZakic <53043742+VanjaZakic@users.noreply.github.com>
2019-09-05 20:49:40 +0200

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,7 @@
+U startu sam se striktno pridržavala teksta zadatka.
+Međutim, pošto je u uputstvu navedeno da je moguće još nešto dodatno ubaciti, onda sam sebi dala slobodu na napravim još neke fajlove.
+connection.php je napravljen kako ne bih u svakom fajlu pisala konekciju.
+welcome.php nije bio definisan zadatkom, ali sam to napravila pošto mi je bilo logično da se nakon logovanja korisnik preusmeri na novu stranicu (ta stranica bi naravno trebalo da sadrži još dosta sadržaja u skladu sa svrhom sajta).
+Link log out je stavljen zato što se inače primenjuje.
+Iz teksta zadatka sam shvatila da search forma treba da postoji i pre neko da se ulogujemo, a i nakon toga pa sam je zato ubacila u na home screen i na welcome screen.
+search.php sam smatrala da je poželjan pošto se pojavljuje na dva mesta.
